7c4bbd3849a2e9904229ace515641c89b39748b7
JesusKid <frawllings@gmail.com>
2023-03-07 14:06:48 +0100

    message

--- server/index.js
+++ server/index.js
@@ -6,17 +6,23 @@ import postRoutes from './routes/postRoutes.js'
 import dalle from './routes/dalleRoutes.js'
 import creditsRoutes from "./routes/creditsRoutes.js"
 import accountRoutes from './routes/accountRoutes.js'
+import { checkOrigin } from './middleware/index.js';
+
 
 dotenv.config();
 
 const app = express();
 
+
+
 app.use(cors())
+app.use(checkOrigin)
 app.use(express.json({ limit: '50mb' }))
 app.use('/api/v1/post', postRoutes)
 app.use('/api/v1/dalle', dalle)
 app.use('/api/v1/credits', creditsRoutes)
 app.use('/api/v1/accounts', accountRoutes)
+
 
 app.get('/', async (req, res) => {
     res.send('Hello from Dalle')
---
+++ server/middleware/index.js
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv'
+const allowedOrigins = process.env.ENV ? ['http://localhost:5173'] : ["https://alchemy-university-five.vercel.app"];
+
+export function checkOrigin(req, res, next) {
+    // Get the origin header of the request
+    var origin = req.headers.origin;
+    // If the origin header is not present, assume it is the same as the host header
+    if (!origin) {
+        origin = req.protocol + '://' + req.headers.host;
+    }
+    // Check if the origin is in the allowed origins array
+    if (allowedOrigins.indexOf(origin) > -1) {
+        // If yes, call the next middleware function
+        next();
+    } else {
+        // If no, send a 403 forbidden response
+        res.status(403).send('Access denied. Origin not allowed.');
+    }
+}
